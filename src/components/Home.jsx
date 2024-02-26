028d9a2daba010347609a52802be1519afbd1ed5
9Jassim <jassim9.a@gmail.com>
2024-02-26 21:23:24 +0300

    render rides on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import Ride from './Ride'
 
 const Home = () => {
   const [rides, setRides] = useState([])
@@ -16,6 +17,11 @@ const Home = () => {
   return (
     <div>
       <h1>Rides</h1>
+      <div className="rides-div">
+        {rides.map((ride) => (
+          <Ride key={ride._id} ride={ride} />
+        ))}
+      </div>
     </div>
   )
 }
