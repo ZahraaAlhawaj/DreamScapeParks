d80b0057d05637d102da19c80fff50dbd08672bf
9Jassim <jassim9.a@gmail.com>
2024-02-28 09:59:33 +0300

    fix delete function

--- src/components/PurchaseTicket.jsx
+++ src/components/PurchaseTicket.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const purchaseTicket = () => {
+  let navigate = useNavigate()
   let ticket = {
     personName: '',
     email: '',
@@ -53,6 +55,7 @@ const purchaseTicket = () => {
 
     e.target.reset()
     setCheckedFoods({})
+    navigate('/tickets')
   }
 
   const handleFoodChange = (e) => {
